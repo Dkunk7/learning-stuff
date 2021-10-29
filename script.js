ecdff21b60d45aa4d55fd063f5030284e2f211f1
Dylan K <drkunkel7@gmail.com>
2021-10-29 14:29:09 -0500

    intro to js, html, css

---
+++ script.js
@@ -0,0 +1,21 @@
+const myVariable = 1;
+const myVariable2 = "hello world!";
+const myVariable3 = false;
+const myVariable4 = [1, 2, "three", "four", 5, "six"];
+
+let newVar = 3
+
+function newFunction(number) {
+    if (number === 2) {
+        console.log("It's 2")
+    } else {
+      console.log("It's not 2")
+    }
+};
+
+for (let i = 0; i < myVariable4.length; i++) {
+  console.log(myVariable4[i]);
+}
+
+
+// newFunction(2);
